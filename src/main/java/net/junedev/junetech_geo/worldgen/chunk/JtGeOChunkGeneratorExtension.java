cdce276b826e3a42a2fde7dcd8e10d84499516fb
jimmybobjim <hudsonbaker602@gmail.com>
2025-02-19 19:37:04 +1100

    Add Correct Parameters for JtGeOChunkGeneratorExtension#generateStrata

--- src/main/java/net/junedev/junetech_geo/worldgen/chunk/JtGeOChunkGeneratorExtension.java
+++ src/main/java/net/junedev/junetech_geo/worldgen/chunk/JtGeOChunkGeneratorExtension.java
@@ -1,10 +1,14 @@
 package net.junedev.junetech_geo.worldgen.chunk;
 
 import net.junedev.junetech_geo.JunetechGeo;
+import net.junedev.junetech_geo.worldgen.settings.WorldStrataOptions;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.blending.Blender;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 // TODO: literally all of this
@@ -13,10 +17,9 @@ public interface JtGeOChunkGeneratorExtension {
         return (JtGeOChunkGeneratorExtension) generator;
     }
 
-    // TODO: all of these parameters, need to figure out what ones are necessary for this
-    default CompletableFuture<ChunkAccess> generateStrata(ChunkAccess chunk) {
-        JunetechGeo.LOGGER.info("Generating strata in chunk {}", chunk.getPos());
+    default CompletableFuture<ChunkAccess> generateStrata(Executor pExecutor, Blender pBlender, RandomState pRandom, ChunkAccess pChunk, WorldStrataOptions options) {
+        JunetechGeo.LOGGER.info("Generating strata in chunk {}", pChunk.getPos());
         // Simulate some loading time
-        return CompletableFuture.supplyAsync(() -> chunk, CompletableFuture.delayedExecutor(2, TimeUnit.SECONDS));
+        return CompletableFuture.supplyAsync(() -> pChunk, CompletableFuture.delayedExecutor(2, TimeUnit.SECONDS));
     }
 }
